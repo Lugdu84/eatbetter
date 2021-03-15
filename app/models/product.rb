5d8e0abbb3cfbc5fc6f0e7088c8cb0ba049513cf
David Grammatico <david.lugdunum@gmail.com>
2021-03-15 18:05:47 +0100

    add product v2

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :farm
+  has_many_attached :photos
   validates :name, presence: true
   validates :price, presence: true
   validates :units, presence: true
