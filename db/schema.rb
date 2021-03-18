aabdcdbdaeeb94d2f41865160dfcd7e05217e150
David Grammatico <david.lugdunum@gmail.com>
2021-03-18 17:41:14 +0100

    add content to product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_18_110931) do
+ActiveRecord::Schema.define(version: 2021_03_18_164036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2021_03_18_110931) do
     t.bigint "farm_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "content"
     t.index ["farm_id"], name: "index_products_on_farm_id"
   end
 
