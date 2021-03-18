487ca3f50f328b11e90276869f3b640900d3ce09
David Grammatico <david.lugdunum@gmail.com>
2021-03-18 21:41:25 +0100

    add model favorites

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_18_203423) do
+ActiveRecord::Schema.define(version: 2021_03_18_204038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,15 @@ ActiveRecord::Schema.define(version: 2021_03_18_203423) do
     t.string "photo3"
     t.string "photo_owner"
     t.index ["user_id"], name: "index_farms_on_user_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "farm_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["farm_id"], name: "index_favorites_on_farm_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -123,6 +132,8 @@ ActiveRecord::Schema.define(version: 2021_03_18_203423) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "farms", "users"
+  add_foreign_key "favorites", "farms"
+  add_foreign_key "favorites", "users"
   add_foreign_key "products", "farms"
   add_foreign_key "reviews", "farms"
   add_foreign_key "reviews", "users"
