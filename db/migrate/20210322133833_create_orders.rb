bc717ef18e4816849b18af45b5cff0ec4f9041d2
David Grammatico <david.lugdunum@gmail.com>
2021-03-22 14:44:01 +0100

    model order and order_item

---
+++ db/migrate/20210322133833_create_orders.rb
@@ -0,0 +1,12 @@
+class CreateOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orders do |t|
+      t.boolean :validate
+      t.datetime :date
+      t.float :amount
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
