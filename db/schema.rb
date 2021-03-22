8da89b14b6d72ad8b8c07b1d67f6a1bc7864196a
David Grammatico <david.lugdunum@gmail.com>
2021-03-22 14:55:33 +0100

    migration order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_065614) do
+ActiveRecord::Schema.define(version: 2021_03_22_134158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,29 @@ ActiveRecord::Schema.define(version: 2021_03_22_065614) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["market_id"], name: "index_openings_on_market_id"
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "market_id", null: false
+    t.bigint "order_id", null: false
+    t.integer "quantity"
+    t.datetime "date_collect"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["market_id"], name: "index_order_items_on_market_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.boolean "validate"
+    t.datetime "date"
+    t.float "amount"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -158,6 +181,10 @@ ActiveRecord::Schema.define(version: 2021_03_22_065614) do
   add_foreign_key "favorites", "users"
   add_foreign_key "markets", "farms"
   add_foreign_key "openings", "markets"
+  add_foreign_key "order_items", "markets"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "products"
+  add_foreign_key "orders", "users"
   add_foreign_key "products", "farms"
   add_foreign_key "reviews", "farms"
   add_foreign_key "reviews", "users"
