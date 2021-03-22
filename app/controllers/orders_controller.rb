6d0e735639859a13db6746647a27e80bfd99bbdf
David Grammatico <david.lugdunum@gmail.com>
2021-03-22 20:12:55 +0100

    debug initautocomplete and begin order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   def show
-    @order = current_user.orders.find(params[:id])
+    @order = current_user.orders.first
+    @order_items = @order.order_items
   end
 
   def index
