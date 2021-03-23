93eb4e7d8c00ccb0e0d0196dee7fda94e9afb240
David Grammatico <david.lugdunum@gmail.com>
2021-03-23 20:44:05 +0100

    add item-order in order, and delete item-order in index order

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,14 +1,20 @@
 class OrderItemsController < ApplicationController
+
+
+  def destroy
+    @order_item = OrderItem.find(params[:id])
+    @order_item.destroy
+    redirect_to orders_path
+  end
+
   def create
-    @order_item = OrderItem.new(order_item_params)
-    raise
-    @order_item.save
+    @order_item = OrderItem.create!(order_items_params)
     redirect_to orders_path
   end
 
   private
 
-  def order_item_params
-    params.require(:order_items).permit(:product, :market, :quantity, :order, :date_collect)
+  def order_items_params
+    params.require(:order_item).permit(:product_id, :market_id, :quantity, :order_id)
   end
 end
