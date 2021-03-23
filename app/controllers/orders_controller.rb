d3b5937786a57d910b57017c351e905119a65f7c
David Grammatico <david.lugdunum@gmail.com>
2021-03-23 23:13:20 +0100

    views order and delete if

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,20 +5,9 @@ class OrdersController < ApplicationController
     @products = Product.select { |m| m.farm == @farm }
     @markets = Market.select { |m| m.farm == @farm }
     @order_items = OrderItem.all
-=begin
-    @order_items = []
-    @order_items << OrderItem.create!(
-      order: Order.first,
-      product: @products[0],
-      quantity: 3,
-      market: @markets.first
-    )
-    @order_items << OrderItem.create!(
-      order: Order.first,
-      product: @products[1],
-      quantity: 5,
-      market: @markets.first
-      )
-=end
+    @sum = 0
+    @order_items.each do |item|
+      @sum += item.quantity * item.product.price
+    end
   end
 end
