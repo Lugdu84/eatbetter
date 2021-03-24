a09e8d968a31cc10bde68150c43815312141302e
David Grammatico <david.lugdunum@gmail.com>
2021-03-24 07:50:50 +0100

    initCarjs in function for debug and in the end

--- app/javascript/components/cart.js
+++ app/javascript/components/cart.js
@@ -1,18 +1,22 @@
-const up = document.querySelector("#up");
-const down = document.querySelector("#down");
-const displayQuantity = document.querySelector("#display-quantity");
-let integerQuantity = Number(displayQuantity.innerText);
-up.addEventListener("click", event => {
-  if (integerQuantity <= 19) {
-  integerQuantity += 1;
-  displayQuantity.innerText = integerQuantity.toString();
-  }
-})
-down.addEventListener("click", event => {
-  if (integerQuantity >= 2) {
-  integerQuantity -= 1;
-  displayQuantity.innerText = integerQuantity.toString();
-  }
-})
+const initCart = () => {
+  const up = document.querySelector("#up");
+  const down = document.querySelector("#down");
+  const displayQuantity = document.querySelector("#display-quantity");
+  let integerQuantity = Number(displayQuantity.innerText);
+  up.addEventListener("click", event => {
+    if (integerQuantity <= 19) {
+      integerQuantity += 1;
+      displayQuantity.innerText = integerQuantity.toString();
+    }
+  })
+  down.addEventListener("click", event => {
+    if (integerQuantity >= 2) {
+      integerQuantity -= 1;
+      displayQuantity.innerText = integerQuantity.toString();
+    }
+  })
+}
+
+export { initCart };
 
 
