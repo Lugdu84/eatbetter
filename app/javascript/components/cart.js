29ec1ebd0ca337d34b3a20970379cc6831f77ddd
David Grammatico <david.lugdunum@gmail.com>
2021-03-24 12:30:34 +0100

    total orderitem

--- app/javascript/components/cart.js
+++ app/javascript/components/cart.js
@@ -4,19 +4,26 @@ const initCart = () => {
   const displayQuantity = document.querySelector("#display-quantity");
   if (displayQuantity) {
     const numberValue = document.getElementById('myNumber');
+    const productPrice = document.getElementById('price-product');
+    const totalPrice = document.getElementById('total-item');
     let integerQuantity = Number(displayQuantity.innerText);
+    let integerProductPrice = Number(productPrice.innerText);
     up.addEventListener("click", event => {
       if (integerQuantity <= 19) {
         integerQuantity += 1;
+        let total = Math.round((integerQuantity * integerProductPrice) * 100) / 100;
         displayQuantity.innerText = integerQuantity.toString();
-        numberValue.value = integerQuantity
+        numberValue.value = integerQuantity;
+        totalPrice.innerText = `TOTAL : ${total.toString()}`;
       }
     })
     down.addEventListener("click", event => {
       if (integerQuantity >= 2) {
         integerQuantity -= 1;
+        let total = Math.round((integerQuantity * integerProductPrice) * 100) / 100;
         displayQuantity.innerText = integerQuantity.toString();
         numberValue.value = integerQuantity;
+        totalPrice.innerText = `TOTAL : ${total.toString()}`;
       }
     })
   }
