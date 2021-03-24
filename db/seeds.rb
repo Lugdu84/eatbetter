26b2122049435a49a0122aef5d1d7da21d9f5e3b
David Grammatico <david.lugdunum@gmail.com>
2021-03-24 11:33:59 +0100

    seed with order

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
-OrderItem.destroy_all
-Order.destroy_all
 puts "Destroy all favorites..."
 Favorite.destroy_all
 puts "Destroy all reviews..."
